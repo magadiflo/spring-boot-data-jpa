0fcfec8ca16a1fa1f98b4f612517972bcb6f6778
Martín <magadiflo@gmail.com>
2022-03-23 20:06:25 -0500

    Cargando imagen programáticamente en la respuesta HTTP - con ResponseEntity

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,20 +1,20 @@
 package com.bolsadeideas.springboot.app;
 
-import java.nio.file.Paths;
+//import java.nio.file.Paths;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 	
-	private final Logger log = LoggerFactory.getLogger(getClass());
+	//private final Logger log = LoggerFactory.getLogger(getClass());
 
 	// Para agregar recursos estáticos a nuestro proyecto
-	@Override
+	/*@Override >>>>>>> Se cargarán las imágenes programáticamente en la respuesta HTTP
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 
@@ -25,5 +25,6 @@ public class MvcConfig implements WebMvcConfigurer {
 			.addResourceLocations(resourcePath); //Esta es la ruta física donde están las imágenes. La carpeta la creamos en la raíz del proyecto
 
 	}
+	*/
 
 }
