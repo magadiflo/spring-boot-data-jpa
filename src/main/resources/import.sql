bb44a9d7a5ad2902ed8ed33b7f7eb761bb23b735
Martín <magadiflo@gmail.com>
2022-03-30 19:23:22 -0500

    Listando las facturas en la vista de detalle del Cliente - ver.html

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -33,3 +33,13 @@ INSERT INTO productos(nombre, precio, create_at) VALUES('Sony Noteboo Z110', 320
 INSERT INTO productos(nombre, precio, create_at) VALUES('Hewlett Packard Multifuncional F2280', 1500, NOW());
 INSERT INTO productos(nombre, precio, create_at) VALUES('Bianchi Bicicleta Aro 26', 1200, NOW());
 INSERT INTO productos(nombre, precio, create_at) VALUES('Mica Cómoda 5 Cajones', 970, NOW());
+
+/* Populate tabla facturas y sus facturas_items */
+INSERT INTO facturas(descripcion, observacion, cliente_id, create_at) VALUES('Factura de equipos de oficina', null, 1, NOW());
+INSERT INTO facturas_items(cantidad, factura_id, producto_id) VALUES(1,1,1);
+INSERT INTO facturas_items(cantidad, factura_id, producto_id) VALUES(2,1,4);
+INSERT INTO facturas_items(cantidad, factura_id, producto_id) VALUES(1,1,5);
+INSERT INTO facturas_items(cantidad, factura_id, producto_id) VALUES(1,1,7);
+
+INSERT INTO facturas(descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'El precio está fue con descuento', 1, NOW());
+INSERT INTO facturas_items(cantidad, factura_id, producto_id) VALUES(3,2,6);
