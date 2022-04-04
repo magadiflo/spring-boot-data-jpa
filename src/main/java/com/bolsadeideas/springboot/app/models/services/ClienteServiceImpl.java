dab8737e1c1b478f2cfd24c62da58d738ee24bbf
Martín <magadiflo@gmail.com>
2022-04-04 17:57:51 -0500

    Creando CRUD Repository Factura e implementando save en clase Service

--- src/main/java/com/bolsadeideas/springboot/app/models/services/ClienteServiceImpl.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/services/ClienteServiceImpl.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bolsadeideas.springboot.app.models.dao.IClienteDao;
+import com.bolsadeideas.springboot.app.models.dao.IFacturaDao;
 import com.bolsadeideas.springboot.app.models.dao.IProductoDao;
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.entity.Factura;
 import com.bolsadeideas.springboot.app.models.entity.Producto;
 
 @Service
@@ -21,6 +23,9 @@ public class ClienteServiceImpl implements IClienteService {
 	
 	@Autowired
 	private IProductoDao productoDao;
+	
+	@Autowired
+	private IFacturaDao facturaDao;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -56,6 +61,12 @@ public class ClienteServiceImpl implements IClienteService {
 	@Transactional(readOnly = true)
 	public List<Producto> findByNombre(String term) {
 		return this.productoDao.findByNombreLikeIgnoreCase("%" + term + "%");
+	}
+
+	@Override
+	@Transactional
+	public void saveFactura(Factura factura) {
+		this.facturaDao.save(factura);
 	}
 
 }
