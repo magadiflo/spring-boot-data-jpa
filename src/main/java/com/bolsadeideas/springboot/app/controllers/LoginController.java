132c5eed36b0e0b12cda69d5867b85662e499649
Martín <magadiflo@gmail.com>
2022-04-06 19:37:43 -0500

    Validando LoginController con los mensajes de error

--- src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/LoginController.java
@@ -5,17 +5,23 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value="error", required = false) String error, Model model, Principal principal, RedirectAttributes flash) {
 		if (principal != null) {
 			flash.addFlashAttribute("info", "Ya ha iniciado sesión anteriomente");
 			return "redirect:/";
 		}
+		
+		if(error != null) {
+			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta. Por favor vuelva a intentarlo!");
+		}
+		
 		model.addAttribute("titulo", "Iniciar sesión!");
 		return "login";
 	}
