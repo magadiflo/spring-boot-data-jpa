2d620d6da6b0ba026246bdc4895d181975ec6f2d
Martín <magadiflo@gmail.com>
2022-04-07 18:17:50 -0500

    Manejo de errores de acceso denegado AccessDeniedPage

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -5,6 +5,7 @@ package com.bolsadeideas.springboot.app;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 //import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -26,5 +27,9 @@ public class MvcConfig implements WebMvcConfigurer {
 
 	}
 	*/
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/error_403").setViewName("error_403");		
+	}
 
 }
