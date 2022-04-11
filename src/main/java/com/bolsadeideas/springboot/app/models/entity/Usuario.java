bce8551215ace489a1b731f00a95c0e37309cf06
Martín <magadiflo@gmail.com>
2022-04-11 18:51:14 -0500

    CORRECCIÓN: Renombrando clase User a Usuario para serguir sergún el tutorial

--- src/main/java/com/bolsadeideas/springboot/app/models/entity/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/app/models/entity/Usuario.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "users")
-public class User implements Serializable {
+public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
