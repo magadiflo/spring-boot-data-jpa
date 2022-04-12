24dbad58d59f871042f1671ece96c73f2b046ac9
Martín <magadiflo@gmail.com>
2022-04-11 20:04:02 -0500

    Configuración de nuestro sistema multilenguaje y el Locale

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -1,55 +1,50 @@
 package com.bolsadeideas.springboot.app;
 
+import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 
-//import java.nio.file.Paths;
-
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-	
-	//private final Logger log = LoggerFactory.getLogger(getClass());
 
-	// Para agregar recursos estáticos a nuestro proyecto
-	/*@Override >>>>>>> Se cargarán las imágenes programáticamente en la respuesta HTTP
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		WebMvcConfigurer.super.addResourceHandlers(registry);
-
-		String resourcePath = Paths.get("uploads").toAbsolutePath().toUri().toString();
-		log.info("resourcePath: " + resourcePath);
-		registry
-			.addResourceHandler("/uploads/**") //Desde la vista se accede a través de esta ruta, es decir lo configuramos como un directorio static
-			.addResourceLocations(resourcePath); //Esta es la ruta física donde están las imágenes. La carpeta la creamos en la raíz del proyecto
-
-	}
-	*/
-	
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/error_403").setViewName("error_403");		
+		registry.addViewController("/error_403").setViewName("error_403");
 	}
-	
-	/////////////////Este método fue movido de la clase SpringSecurityConfig
-	/////////////////para ver que también puede ser inyectado en esa clase
-	/////////////////con el @Autowired ya que este método tiene la anotación
-	////////////////del tipo @Bean
-	// @Bean: para guardar el objeto creado con new BCryptPasswordEncoder() en el
-	// contenedor. Esto será usado por defecto por Spring Security
-	// >>>> static, se colocó static por que mostraba error de que hay una
-	// dependencia circular
-	// >>>> ya que SpringSecurityConfig es un bean en sí misma que necesita de la
-	// instancia completa
-	// >>>> del bean BCryptPasswordEncoder, y este último necesita a su vez
-	// >>>> una intancia de SpringSecurityConfig
+
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() { //static: Aquí ya no hay problema con la dependencia circular po eso no colocamos static
+	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	// Configuración de nuestro sistema multilenguaje y el Locale: 2 @Bean y un
+	// método para registrar el interceptor LocaleChangeInterceptor
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(new Locale("es", "ES"));
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+		localeInterceptor.setParamName("lang");
+		return localeInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(this.localeChangeInterceptor());
 	}
 
 }
