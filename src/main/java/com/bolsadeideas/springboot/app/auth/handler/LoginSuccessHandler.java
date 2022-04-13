328b88b3411097ff5c5412914c8a038f35ce57f0
Martín <magadiflo@gmail.com>
2022-04-12 19:27:23 -0500

    Agregando traducción en el LoginSuccessHandler

--- src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/bolsadeideas/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -1,19 +1,29 @@
 package com.bolsadeideas.springboot.app.auth.handler;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Autowired
+	private LocaleResolver localeResover;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -21,7 +31,11 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
 		FlashMap flashMap = new FlashMap();
-		flashMap.put("success", "Hola " + authentication.getName() + ", haz iniciado sesión con éxito!");
+		
+		Locale locale = this.localeResover.resolveLocale(request);
+		String mensaje = String.format(this.messageSource.getMessage("text.login.success", null, locale), authentication.getName());
+		
+		flashMap.put("success", mensaje);
 
 		flashMapManager.saveOutputFlashMap(flashMap, request, response);
 		
