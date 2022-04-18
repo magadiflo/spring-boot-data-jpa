69728b876c0dc90656bc780f3837eb49e5a9410f
Martín <magadiflo@gmail.com>
2022-04-18 18:01:42 -0500

    Implementando la clase de vista FacturaXlsxView para la factura

--- src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlsxView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/xlsx/FacturaXlsxView.java
@@ -5,9 +5,14 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractXlsxView;
+
+import com.bolsadeideas.springboot.app.models.entity.Factura;
 
 @Component("factura/ver.xlsx")
 public class FacturaXlsxView extends AbstractXlsxView {
@@ -15,6 +20,33 @@ public class FacturaXlsxView extends AbstractXlsxView {
 	@Override
 	protected void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+
+		Factura factura = (Factura) model.get("factura");
+		Sheet sheet = workbook.createSheet("Factura Spring");
+
+		////////////// 1° Forma agregando fila y columna
+		// 1° Fila 1° Columna
+		Row row = sheet.createRow(0);
+		Cell cell = row.createCell(0);
+		cell.setCellValue("Datos del cliente");
+
+		// 2° Fila 1° Columna
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getNombre().concat(" ").concat(factura.getCliente().getApellido()));
+
+		// 3° Fila 1° Columna
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellValue(factura.getCliente().getEmail());
+
+		///////////// 2° Forma, confinuamos agregando fila y columna.
+		// 5° Fila 1° Columna (Nos saltamos la fila 4 para dar espacio)
+		sheet.createRow(4).createCell(0).setCellValue("Datos de la factura");
+
+		sheet.createRow(5).createCell(0).setCellValue("Folio: " + factura.getId());
+		sheet.createRow(6).createCell(0).setCellValue("Descripción: " + factura.getDescripcion());
+		sheet.createRow(7).createCell(0).setCellValue("Fecha: " + factura.getCreateAt());
 
 	}
 
