43db8fa94876765d17a5ef0b353e9b0f924fbe7f
Martín <magadiflo@gmail.com>
2022-04-18 19:46:53 -0500

    Tener más de una vista con @Component y media types

--- src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
+++ src/main/java/com/bolsadeideas/springboot/app/view/csv/ClienteCsvView.java
@@ -14,7 +14,7 @@ import org.supercsv.prefs.CsvPreference;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-@Component("listar")
+@Component("listar.csv")
 public class ClienteCsvView extends AbstractView {
 
 	public ClienteCsvView() {
